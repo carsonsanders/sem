0f6fac2f0d457b1b198530cc8456e4b03b0e8603
Ahad Malik <ahadm94@hotmail.co.uk>
2019-03-01 15:56:42 +0000

    Country class added, App updated.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -14,10 +14,10 @@ public class App
         a.connect();
 
         // Extract employee salary information
-        ArrayList<Employee> employees = a.getAllSalaries();
+        ArrayList<Country> countries = a.getAllCountries();
 
         // Test the size of the returned data - should be 240124
-        System.out.println(employees.size());
+        System.out.println(countries.size());
 
         // Disconnect from database
         a.disconnect();
@@ -88,32 +88,35 @@ public class App
      * Gets all the current employees and salaries.
      * @return A list of all employees and salaries, or null if there is an error.
      */
-    public ArrayList<Employee> getAllSalaries()
+    public ArrayList<Country> getAllCountries()
     {
         try
         {
             // Create an SQL statement
             Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
-                            + "FROM employees, salaries "
-                            + "WHERE employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' "
-                            + "ORDER BY employees.emp_no ASC";
+            // Query to select all countries
+            String SQL = "SELECT * FROM Country";
             // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
+            ResultSet rs = stmt.executeQuery(SQL);
+
             // Extract employee information
-            ArrayList<Employee> employees = new ArrayList<Employee>();
-            while (rset.next())
+            ArrayList<Country> countryList = new ArrayList<Country>();
+
+            while (rs.next())
             {
-                Employee emp = new Employee();
-                emp.emp_no = rset.getInt("employees.emp_no");
-                emp.first_name = rset.getString("employees.first_name");
-                emp.last_name = rset.getString("employees.last_name");
-                emp.salary = rset.getInt("salaries.salary");
-                employees.add(emp);
+
+                Country count = new Country();
+                count.setCode(rs.getString("Country.Code"));
+                count.setName(rs.getString("Country.Name"));
+                count.setContinent(rs.getString("Country.Continent"));
+                count.setRegion(rs.getString("Country.Region"));
+                count.setPopulation(rs.getInt("Country.Population"));
+                count.setCapital(rs.getString("Country.Capital"));
+
+                //Add country to Arraylist
+                countryList.add(count);
             }
-            return employees;
+            return countryList;
         }
         catch (Exception e)
         {
@@ -122,4 +125,8 @@ public class App
             return null;
         }
     }
+
+
+
+
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,72 @@
+package com.napier.sem;
+
+public class Country {
+
+
+
+    private String code;
+    private String name;
+    private String continent;
+    private String region;
+    private double population;
+    private String capital;
+
+
+    //Default constructor
+    Country(){
+    }
+
+
+    //accessor methods
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public double getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(double population) {
+        this.population = population;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+
+
+
+}
