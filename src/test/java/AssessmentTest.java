cb764fd686f9ad982ff5e206d31d7cd33bd08327
Ahad Malik <ahadm94@hotmail.co.uk>
2019-03-11 16:01:26 +0000

    More unit tests added for Code & Region properties for Country class

--- src/test/java/AssessmentTest.java
+++ src/test/java/AssessmentTest.java
@@ -13,19 +13,44 @@ class AssessmentTest
 
     //testing setters and getters for Code property of the Country class
     @Test
-    void countryCodeTests()
-    {
+    void countryCodeTests() {
         try {
-            testCountry.setName("COL");
+            testCountry.setCode("COL");
             assertEquals("COL", "COL", testCountry.getCode());
         } catch (InputValidationException e) {
             fail("Should not throw an Exception");
         }
+
+        try {
+            testCountry.setCode("PAK");
+        } catch (InputValidationException e) {
+            fail("Should not throw an exception");
+        }
+
+        try {
+            testCountry.setCode("AFGH");
+            fail("Code cannot be more than 3 Characters");
+        } catch (InputValidationException e) {
+        }
+
+        try {
+            testCountry.setCode("SC");
+            fail("Code cannot be less than 3 Characters");
+        } catch (InputValidationException e) {
+        }
+
+        try {
+            testCountry.setCode("Q4T");
+            fail("Code cannot contain numbers");
+        } catch (InputValidationException e) {
+        }
+
+
     }
 
     //testing setters and getters for Name property of the Country class
     @Test
-    public void countryNameTests() throws Exception {
+    public void countryNameTests() {
 
 
         //testing setters and getters
@@ -45,7 +70,7 @@ class AssessmentTest
 
         try {
             testCountry.setName("1Kenya");
-            fail("Name should start with a letter");
+            fail("Name cannot contain numbers");
         } catch (InputValidationException e) {
         }
 
@@ -74,7 +99,7 @@ class AssessmentTest
 
     //testing setters and getters for Continent property for Country class
     @Test
-    public void countryContinentTests() throws Exception {
+    public void countryContinentTests() {
 
 
 
@@ -94,7 +119,7 @@ class AssessmentTest
 
         try {
             testCountry.setContinent("1Asia");
-            fail("Name should start with a letter");
+            fail("Continent cannot contain numbers");
         } catch (InputValidationException e) {
         }
 
@@ -112,12 +137,48 @@ class AssessmentTest
 
         }
 
-
-
     }
 
+    @Test
+    public void countryRegionTests() {
 
 
+
+        try {
+            testCountry.setRegion("Eastern Africa");
+            assertEquals("Eastern Africa", "Eastern Africa", testCountry.getRegion());
+        } catch (InputValidationException e) {
+            fail("Should not throw an Exception");
+        }
+
+
+        try {
+            testCountry.setRegion("Southern Europe");
+        }catch (InputValidationException e ) {
+            fail("Should not throw an exception");
+        }
+
+        try {
+            testCountry.setRegion("6Middle East");
+            fail("Region cannot contain numbers");
+        } catch (InputValidationException e) {
+        }
+
+        try {
+            testCountry.setRegion("A");
+            fail("Should not allow less than two characters");
+        } catch (InputValidationException e) {
+
+        }
+
+        try {
+            testCountry.setRegion("abcdefghijklmnopqrstxyzabcdefgh");
+            fail("Should not be longer than 30 characters");
+        } catch (InputValidationException e) {
+
+        }
+
+    }
 
 
 
