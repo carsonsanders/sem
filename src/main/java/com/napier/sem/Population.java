08c304a55e4f31a08561c13238633b0472d476ca
Ahad Malik <ahadm94@hotmail.co.uk>
2019-05-10 16:37:54 +0100

    Population class added

---
+++ src/main/java/com/napier/sem/Population.java
@@ -0,0 +1,18 @@
+package com.napier.sem;
+
+public class Population {
+
+
+    private int population;
+
+
+    //getters and setters
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+}
