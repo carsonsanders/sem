0129b356495545845134f95f3b6d613b99e4b70f
Ahad Malik <ahadm94@hotmail.co.uk>
2019-05-12 17:41:46 +0100

    Basic menu system implemented to extract country info

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @SpringBootApplication
 @RestController
@@ -26,8 +27,8 @@ public class App {
             a.connect(args[0]);
         }
 
-       PrintReports rep = new PrintReports();
-        rep.countriesInRegionReport("Eastern Asia");
+        a.reportSelection();
+
 
 
     }
@@ -94,9 +95,60 @@ public class App {
         }
     }
 
+    public void reportSelection(){
+
+        PrintReports newRep = new PrintReports();
+        //print welcome screen
+        welcomeScreen();
+
+        Scanner scanner = new Scanner(System.in);
+
+        switch(scanner.next()) {
+
+            //All the countries in the ...
+            case "1":
+
+                clearScreen();
+                countryScreen();
+
+                switch (scanner.next()){
+
+                    case"1": newRep.countriesInWorldReport();
+                             break;
+
+                    case"2": continentScreen();
+
+                         switch (scanner.next()){
+
+                             case "1": newRep.countriesInContinentReport("Africa");
+                                       break;
+                             case "2": newRep.countriesInContinentReport("Asia");
+                                       break;
+                             case "3": newRep.countriesInContinentReport("Europe");
+                                       break;
+                             case "4": newRep.countriesInContinentReport("North America");
+                                       break;
+                             case "5": newRep.countriesInContinentReport("South America");
+                                       break;
+                             case "6": newRep.countriesInContinentReport("Oceania");
+                                 break;
+                         }
+                         break;
+
+                }
+                break;
+
+            //All the cities in the ...
+            case "2":
+        }
+
+    }
+
+
+
     public void welcomeScreen() {
 
-        //Display main menu options
+        clearScreen();
         System.out.println("\n");
         System.out.println("Welcome!");
         System.out.println("\n");
@@ -105,6 +157,48 @@ public class App {
         System.out.println("2. All the cities in...");
         System.out.println("3. All the capital cities in...");
         System.out.println("Press 'x' to exit");
+
+    }
+
+    public void countryScreen() {
+
+        //Display country menu options
+        System.out.println("Would you like a population report on..");
+        System.out.println("1. All the countries in the World");
+        System.out.println("2. All the countries in a Continent");
+        System.out.println("3. All the countries in a Region");
+        System.out.println("Press 'x' to exit");
+
+    }
+
+    public void continentScreen() {
+
+        //Display country menu options
+        System.out.println("Choose a continent :");
+        System.out.println("1. Africa");
+        System.out.println("2. Asia");
+        System.out.println("3. Europe");
+        System.out.println("4. North America");
+        System.out.println("5. South America");
+        System.out.println("6. Oceania");
+
+    }
+
+
+    public void regionScreen() {
+
+        //Display country menu options
+        System.out.println("Choose a region :");
+        System.out.println("1. Africa");
+        System.out.println("2. Asia");
+        System.out.println("3. Europe");
+        System.out.println("4. North America");
+        System.out.println("5. South America");
+        System.out.println("6. Oceania");
+
+    }
+
+    public static void clearScreen() {
 
     }
 
